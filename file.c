83f8a0b87c50ec7c4703dab818d8ec0157a8308c
g1dotcorp <gagan.n@s.amity.edu>
2025-03-03 22:35:37 -0700

    It will only print if it is a even number

---
+++ file.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	printf("Enter the value to check whether the number is even or odd :")
+	scanf("%d", &i);
+	if(i%2==0);
+	{
+		printf("It is an even number");
+	}
+	return 0;
+}
