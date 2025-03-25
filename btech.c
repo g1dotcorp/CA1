f8b173dfe6ba710573cfc59d872dbbebccab2e75
g1dotcorp <gagan.n@s.amity.edu>
2025-03-25 04:58:04 -0700

    third branch commit

--- btech.c
+++ btech.c
@@ -4,27 +4,31 @@ int main() {
     int choice;
 
     printf("Select a course by entering the corresponding number:\n");
-    printf("1. Mathematics\n");
-    printf("2. Physics\n");
-    printf("3. Computer Science\n");
-    printf("4. Chemistry\n");
+    printf("1. CSE\n");
+    printf("2. ECE\n");
+    printf("3. AIML\n");
+    printf("4. mechanical\n");
     printf("Enter your choice (1-4): ");
     scanf("%d", &choice);
 
     switch (choice) {
         case 1:
-            printf("You selected Mathematics.\n");
+            printf("You selected CSE.\n");
             break;
         case 2:
-            printf("You selected Physics.\n");
+            printf("You selected ECE.\n");
             break;
-      
+        case 3:
+            printf("You selected AIML.\n");
+            break;
+   
         default:
             printf("Invalid choice! Please select a number between 1 and 4.\n");
     }
 
     return 0;
 }
+
 
 
 
