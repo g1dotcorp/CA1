61088f6c8f9801118180f3b3565e1519293426f3
g1dotcorp <gagan.n@s.amity.edu>
2025-03-25 04:42:03 -0700

    c program main

---
+++ btech.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+
+    printf("Select a course by entering the corresponding number:\n");
+    printf("1. Mathematics\n");
+    printf("2. Physics\n");
+    printf("3. Computer Science\n");
+    printf("4. Chemistry\n");
+    printf("Enter your choice (1-4): ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            printf("You selected Mathematics.\n");
+            break;
+        
+        default:
+            printf("Invalid choice! Please select a number between 1 and 4.\n");
+    }
+
+    return 0;
+}
+
